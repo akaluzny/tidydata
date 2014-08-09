7467a069d70574d3105a6ea1bc778ed276677922
Alex K <akaluzny@gmail.com>
2014-08-09 17:13:55 -0500

    Removed old functions

--- run_analysis.R
+++ run_analysis.R
@@ -1,33 +1,3 @@
-ObtainFiles <- function() {
-  download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-                destfile ="getdata-projectfiles-UCI HAR Dataset.zip", method = "curl")
-  unzip("getdata-projectfiles-UCI HAR Dataset.zip")
-}
-
-MergeTrainingAndTest <- function() {
-  train.X.data <- read.table("UCI HAR Dataset/train/X_train.txt")
-  test.X.data <- read.table("UCI HAR Dataset/test/X_test.txt")
-  rbind(train.X.data, test.X.data)
-}
-
-ExtractMeanAndStd <- function(data) {
-  features <- read.table("UCI HAR Dataset/features.txt")[[2]]
-  names(data) <- features
-  have.mean.or.std <- grep("mean\\(\\)|std()", features)
-  data[, have.mean.or.std]
-}
-
-UseDescriptiveActivityNames <- function(data) {
-  train.y.data <- read.table("UCI HAR Dataset/train/y_train.txt")
-  test.y.data <- read.table("UCI HAR Dataset/test/y_test.txt")
-  y.data <- rbind(train.y.data, test.y.data)
-  
-  activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt")
-  activities <- merge(y.data, activity.labels, by.y = "V1")
-  data$Activity <- activities[[2]]
-  data
-}
-
 RunAnalysis <- function() {
   # Download file from given location and unzip its contents
   download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
