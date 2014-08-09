a98bd3770107e076765bc7bb2298c57fa7b17ce3
Alex K <akaluzny@gmail.com>
2014-08-09 14:50:20 -0500

    3. Uses descriptive activity names to name the activities in the data set.

--- run_analysis.R
+++ run_analysis.R
@@ -5,9 +5,9 @@ ObtainFiles <- function() {
 }
 
 MergeTrainingAndTest <- function() {
-  train.x.data <- read.table("UCI HAR Dataset/train/X_train.txt")
-  test.x.data <- read.table("UCI HAR Dataset/test/X_test.txt")
-  rbind(train.x.data, test.x.data)
+  train.X.data <- read.table("UCI HAR Dataset/train/X_train.txt")
+  test.X.data <- read.table("UCI HAR Dataset/test/X_test.txt")
+  rbind(train.X.data, test.X.data)
 }
 
 ExtractMeanAndStd <- function(data) {
@@ -15,4 +15,15 @@ ExtractMeanAndStd <- function(data) {
   names(data) <- features
   have.mean.or.std <- grep("mean\\(\\)|std()", features)
   data[, have.mean.or.std]
+}
+
+UseDescriptiveActivityNames <- function(data) {
+  train.y.data <- read.table("UCI HAR Dataset/train/y_train.txt")
+  test.y.data <- read.table("UCI HAR Dataset/test/y_test.txt")
+  y.data <- rbind(train.y.data, test.y.data)
+  
+  activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+  activities <- merge(y.data, activity.labels, by.y = "V1")
+  data$Activity <- activities[[2]]
+  data
 }
